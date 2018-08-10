5e1885a6b1e2c39edea91cf37207c6963621734c
flonad <florian.nadaud@serli.com>
2018-08-10 10:35:37 +0200

    add notification for android

--- step-3/App.js
+++ step-3/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AppNavigation from './src/navigation/AppNavigation';
 
+console.ignoredYellowBox = ['Warning: isMounted(...) is deprecated'];
+
 export default class App extends Component {
   
   render() {
---
+++ step-4/src/api/constants.js
@@ -0,0 +1,5 @@
+export const THE_MOVIE_DB = {
+    key: '47b182c34667717b6a81dfddc8643607',
+    url: 'https://api.themoviedb.org/3/',
+    image: 'https://image.tmdb.org/t/p/w500'
+}
